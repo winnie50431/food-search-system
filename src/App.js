dade0ca5015e12b859545538930c7a03c88b25c1
Winnie Lin <winnie50431@gmail.com>
2021-12-19 00:23:45 +0800

    get data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Cards from "./components/Cards";
 import "./styles/style.css";
 
 function App() {
+  const [city, setCity] = useState(null);
+  const [town, setTown] = useState(null);
+  const [places, setPlaces] = useState([]);
+  const url =
+    "https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx";
+
+  const getData = async () => {
+    try {
+      let res = await fetch(url);
+      let data = await res.json();
+      console.log(data);
+      setPlaces(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // useEffect(() => {
+
+  // }, [places]);
+
   return (
     <div className="App">
       <Header />
-      <Cards />
+      <Cards places={places} />
     </div>
   );
 }
